872b61da444788bb39c00633e2fa2448cd7a348e
Purva-Joshi-043 <purvaj043@gmail.com>
2022-06-09 15:34:50 +0530

    updated the migration

--- src/migrations/1654769046619-UserMigration.ts
+++ src/migrations/1654769046619-UserMigration.ts
@@ -1,7 +1,7 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class UserMigration1654751598895 implements MigrationInterface {
-    name = 'UserMigration1654751598895'
+export class UserMigration1654769046619 implements MigrationInterface {
+    name = 'UserMigration1654769046619'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "user" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "username" character varying NOT NULL, "password" character varying NOT NULL, CONSTRAINT "UQ_78a916df40e02a9deb1c4b75edb" UNIQUE ("username"), CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
